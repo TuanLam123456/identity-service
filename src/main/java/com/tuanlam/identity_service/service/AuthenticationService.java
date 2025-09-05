c7c4fb1a042f5ba52f96a3091f9ec91dd7412cc0
TuanLam123456 <130367417+TuanLam123456@users.noreply.github.com>
2025-09-05 14:42:46 +0700

    Authorization by JWT in Spring Security

--- src/main/java/com/tuanlam/identity_service/service/AuthenticationService.java
+++ src/main/java/com/tuanlam/identity_service/service/AuthenticationService.java
@@ -9,6 +9,7 @@ import com.tuanlam.identity_service.dto.request.AuthenticationRequest;
 import com.tuanlam.identity_service.dto.request.IntrospectRequest;
 import com.tuanlam.identity_service.dto.response.AuthenticationResponse;
 import com.tuanlam.identity_service.dto.response.IntrospectResponse;
+import com.tuanlam.identity_service.entity.User;
 import com.tuanlam.identity_service.excpetion.AppException;
 import com.tuanlam.identity_service.excpetion.ErrorCode;
 import com.tuanlam.identity_service.repository.UserRepository;
@@ -21,11 +22,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.StringJoiner;
 
 @Slf4j
 @Service
@@ -60,16 +63,18 @@ public class AuthenticationService {
         if (!authenticated)
             throw new AppException(ErrorCode.UNAUTHENTICATED);
 
-        var token = generateToken(request.getUsername());
+        var token = generateToken(user);
         return AuthenticationResponse.builder()
                 .token(token)
                 .authenticated(true)
                 .build();
     }
-    private String generateToken(String username){
+
+    private String generateToken(User user){
         JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
-                .subject(username)
+                .subject(user.getUsername())
                 .issuer("TuanLam")
                 .issueTime(new Date())
                 .expirationTime(
@@ -77,7 +82,7 @@ public class AuthenticationService {
                                 Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
                         )
                 )
-                .claim("customClaim","Custom")
+                .claim("scope",buildScope(user))
                 .build();
         Payload payload = new Payload(jwtClaimsSet.toJSONObject());
         JWSObject jwsObject = new JWSObject(header,payload);
@@ -88,5 +93,12 @@ public class AuthenticationService {
             log.error("Cannot create token");
             throw new RuntimeException(e);
         }
+    }
+
+    private String buildScope(User user){
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        if (!CollectionUtils.isEmpty(user.getRoles()))
+            user.getRoles().forEach(stringJoiner::add);
+        return stringJoiner.toString();
     }
 }
